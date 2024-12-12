807710fd91768a71a00b4568d8fcc4d455f5ec7d
Daniel <dsilvaa@unicartagena.edu.co>
2024-12-12 13:29:18 -0500

    Textfields added

--- lib/Start/appmain.dart
+++ lib/Start/appmain.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'dart:math';
 
 class MymainApp extends StatefulWidget {
   const MymainApp({super.key, required this.title});
@@ -15,6 +16,10 @@ class _MymainAppState extends State<MymainApp> {
   final TextEditingController _controllerB = TextEditingController();
   final TextEditingController _controllerC = TextEditingController();
   final TextEditingController _controllerT = TextEditingController();
+  final TextEditingController _controllerAI = TextEditingController();
+  final TextEditingController _controllerBI = TextEditingController();
+  final TextEditingController _controllerCI = TextEditingController();
+  final TextEditingController _controllerTI = TextEditingController();
 
   double? resultado; // Variable para almacenar el resultado del cálculo
 
@@ -25,7 +30,7 @@ class _MymainAppState extends State<MymainApp> {
       body: Align(
         alignment: Alignment.centerLeft,
         child: Column(
-          children: [
+          children: [SizedBox(height: 50),
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Wrap(
@@ -34,6 +39,7 @@ class _MymainAppState extends State<MymainApp> {
                 style: GoogleFonts.orbitron(textStyle: TextStyle(color: const Color.fromARGB(255, 251, 251, 251))),
                 keyboardType: TextInputType.number, // Solo permite números
                 decoration: InputDecoration(
+                  labelStyle: GoogleFonts.orbitron(),
                   labelText: 'ANTOINE CONSTANT A',
                   border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
                 ),
@@ -45,16 +51,19 @@ class _MymainAppState extends State<MymainApp> {
                 style: GoogleFonts.orbitron(textStyle: TextStyle(color: const Color.fromARGB(255, 251, 251, 251))),
                 keyboardType: TextInputType.number, // Solo permite números
                 decoration: InputDecoration(
+                  labelStyle: GoogleFonts.orbitron(),
                   labelText: 'ANTOINE CONSTANT B',
                   border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
                 ),
               )
               ),
+              SizedBox(height: 15),
               SizedBox(width: 250, child: TextField(
                 controller: _controllerC, // Asocia el TextEditingController
                 style: GoogleFonts.orbitron(textStyle: TextStyle(color: const Color.fromARGB(255, 251, 251, 251))),
                 keyboardType: TextInputType.number, // Solo permite números
                 decoration: InputDecoration(
+                  labelStyle: GoogleFonts.orbitron(),
                   labelText: 'ANTOINE CONSTANT C',
                   border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
                 ),
@@ -66,32 +75,98 @@ class _MymainAppState extends State<MymainApp> {
                 style: GoogleFonts.orbitron(textStyle: TextStyle(color: const Color.fromARGB(255, 251, 251, 251))),
                 keyboardType: TextInputType.number, // Solo permite números
                 decoration: InputDecoration(
+                  labelStyle: GoogleFonts.orbitron(),
                   labelText: 'SATURATION TEMPERATURE',
                   border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
                 ),
               )
               )
               ],
+              ),
+              SizedBox(width: 15),
+              Column(children: [SizedBox(width: 250, child: TextField(
+                controller: _controllerAI, // Asocia el TextEditingController
+                style: GoogleFonts.orbitron(textStyle: TextStyle(color: const Color.fromARGB(255, 251, 251, 251))),
+                keyboardType: TextInputType.number, // Solo permite números
+                decoration: InputDecoration(
+                  labelStyle: GoogleFonts.orbitron(),
+                  labelText: 'CONSTANT A II',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                ),
               )
+              ), 
+              SizedBox(height: 15),
+              SizedBox(width: 250, child: TextField(
+                controller: _controllerBI, // Asocia el TextEditingController
+                style: GoogleFonts.orbitron(textStyle: TextStyle(color: const Color.fromARGB(255, 251, 251, 251))),
+                keyboardType: TextInputType.number, // Solo permite números
+                decoration: InputDecoration(
+                  labelStyle: GoogleFonts.orbitron(),
+                  labelText: 'CONSTANT B II',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                ),
+              )
+              ),
+              SizedBox(height: 15),
+              SizedBox(width: 250, child: TextField(
+                controller: _controllerCI, // Asocia el TextEditingController
+                style: GoogleFonts.orbitron(textStyle: TextStyle(color: const Color.fromARGB(255, 251, 251, 251))),
+                keyboardType: TextInputType.number, // Solo permite números
+                decoration: InputDecoration(
+                  labelStyle: GoogleFonts.orbitron(),
+                  labelText: 'CONSTANT C II',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                ),
+              )
+              ), 
+              SizedBox(height: 15),
+              SizedBox(width: 250, child: TextField(
+                controller: _controllerTI, // Asocia el TextEditingController
+                style: GoogleFonts.orbitron(textStyle: TextStyle(color: const Color.fromARGB(255, 251, 251, 251))),
+                keyboardType: TextInputType.number, // Solo permite números
+                decoration: InputDecoration(
+                  labelStyle: GoogleFonts.orbitron(),
+                  labelText: 'SATURATION TEMPERATURE II',
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+                ),
+              )
+              )
+              ],)
               ],
               ),
             ),
+            SizedBox(height: 15),
             ElevatedButton(
               onPressed: () {
                 // Convierte el texto a número (si es válido)
                 double? valorA = double.tryParse(_controllerA.text);
                 double? valorB = double.tryParse(_controllerB.text);
                 double? valorC = double.tryParse(_controllerA.text);
-                double? valorT = double.tryParse(_controllerB.text);
+                double? valorT = double.tryParse(_controllerT.text);
+                double? valorAI = double.tryParse(_controllerAI.text);
+                double? valorBI = double.tryParse(_controllerBI.text);
+                double? valorCI = double.tryParse(_controllerAI.text);
+                double? valorTI = double.tryParse(_controllerTI.text);
 
-                setState(() {
-                    // Muestra un mensaje si el valor no es un número válido
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text("Por favor ingrese un número válido")),
-                  ); // Un cálculo de ejemplo
-                  });
-                },  
-              child: Text("Calcular"),
+                if (valorA != null && valorB !=null && valorC != null && valorT != null && valorAI != null && valorBI !=null && valorCI != null && valorTI != null){
+                  setState(() {
+
+                  // ignore: unused_local_variable, non_constant_identifier_names
+                  double PsatI = exp(valorA - (valorB/(valorT+valorC)));
+                  // ignore: unused_local_variable, non_constant_identifier_names
+                  double PsatII = exp(valorAI - (valorBI/(valorTI+valorCI)));
+                  
+                  },);
+                }
+                
+                
+
+                },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30)
+              ),  
+              child: Text("Calcular", style: GoogleFonts.orbitron()),
             ),
             if (resultado != null)
               Text(
